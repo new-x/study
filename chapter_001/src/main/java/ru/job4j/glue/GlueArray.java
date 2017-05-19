82b3532fadf6b8025cacf1d0d9437a9513db39aa
Kirill Alekseev <alexfunny@live.com>
2017-05-19 22:08:12 +0400

    update control task

--- chapter_001/src/main/java/ru/job4j/glue/GlueArray.java
+++ chapter_001/src/main/java/ru/job4j/glue/GlueArray.java
@@ -16,19 +16,49 @@ public class GlueArray {
   public int[] glue(int[] first, int[] second) {
     int[] array = new int[first.length + second.length];
     int count = 0;
+
+
+
     for (int index = 0; index < array.length; index++) {
+      if (first.length > count & second.length > count) {
+        if (first[count] > second[count]) {
+          array[index] = second[count];
+          array[index + 1] = first[count];
+        } else {
+          array[index] = first[count];
+          array[index + 1] = second[count];
+          }
+          count++;
+          index++;
+    } else if (first.length > count) {
+      array[index] = first[count];
+      count++;
+    } else if (second.length > count) {
+      array[index] = second[count];
+      count++;
+    }
+  }
+    /*for (int index = 0; index < array.length; index++) {
       if (count < first.length) {
       if (first[count] > second[count]) {
         array[index] = second[count];
+      }
+      } else {
+        array[index] = second[count];
+      }
+
+      if (count < second.length) {
+      if (second[count] < first[count]) {
         array[index + 1] = first[count];
+      }
       } else {
         array[index] = first[count];
-        array[index + 1] = second[count];
-        }
       }
       count++;
       index++;
+
     }
+    */
     return array;
   }
 }
--- chapter_001/src/test/java/ru/job4j/glue/GlueArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/glue/GlueArrayTest.java
@@ -16,13 +16,69 @@ public class GlueArrayTest {
   */
 
   @Test
-  public void arrayGlue() {
+  public void arrayGlueOne() {
     int[] first = {1, 3, 5, 7, 9};
     int[] second = {2, 4, 6, 8, 10};
   	GlueArray glueArray = new GlueArray();
   	int[] resultArray = glueArray.glue(first, second);
   	int[] expectArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   	assertThat(resultArray, is(expectArray));
+  }
+
+  /**
+  * Test - тестируем склеивание двух массивов, второй массив пустой.
+  */
+
+  @Test
+  public void arrayGlueTwo() {
+    int[] first = {1, 3, 5, 7, 9};
+    int[] second = {};
+    GlueArray glueArray = new GlueArray();
+    int[] resultArray = glueArray.glue(first, second);
+    int[] expectArray = {1, 3, 5, 7, 9};
+    assertThat(resultArray, is(expectArray));
+  }
+
+  /**
+  * Test - тестируем склеивание двух массивов, первый массив пустой.
+  */
+
+  @Test
+  public void arrayGlueThree() {
+    int[] first = {};
+    int[] second = {2, 4, 6, 8, 10};
+    GlueArray glueArray = new GlueArray();
+    int[] resultArray = glueArray.glue(first, second);
+    int[] expectArray = {2, 4, 6, 8, 10};
+    assertThat(resultArray, is(expectArray));
+  }
+
+  /**
+  * Test - тестируем склеивание двух массивов, первый короче.
+  */
+
+  @Test
+  public void arrayGlueFour() {
+    int[] first = {1, 3, 7, 9};
+    int[] second = {2, 4, 6, 8, 10};
+  	GlueArray glueArray = new GlueArray();
+  	int[] resultArray = glueArray.glue(first, second);
+  	int[] expectArray = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+    assertThat(resultArray, is(expectArray));
+  }
+
+  /**
+  * Test - тестируем склеивание двух массивов, второй короче.
+  */
+
+  @Test
+  public void arrayGlueFive() {
+    int[] first = {1, 3, 5, 7, 9};
+    int[] second = {2, 4, 6, 8};
+    GlueArray glueArray = new GlueArray();
+    int[] resultArray = glueArray.glue(first, second);
+    int[] expectArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    assertThat(resultArray, is(expectArray));
   }
 
 }
