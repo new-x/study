0469daf55ca1fa09d5749346aa44598d696c2afb
Kirill Alekseev <alekseev.kirill@outlook.com>
2017-06-06 22:55:10 +0400

    update #2 task #396 update tracker and trackertest

--- chapter_002/src/main/java/ru/job4j/tracker/start/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/start/Tracker.java
@@ -1,22 +1,78 @@
 package ru.job4j.tracker.start;
 import ru.job4j.tracker.models.*;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
- * Created by Nastasia on 04.06.2017.
+ * Class Tracker.
+ * @author alekseev
+ * @since 1.0
  */
+
 public class Tracker {
 
     private  Item[] items = new Item[10];
     private int position = 0;
     private static final Random RN = new Random();
-
+    /**
+     * Метод add  добавление заявок в трекер.
+     * @param item - объект item.
+     * @return возвращаем item.
+     */
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[position++] = item;
         return item;
     }
-
+    /**
+     * Метод delete  удаляет объект из массива и обрезает его.
+     * @param item - объект item.
+     * @return возвращаем item.
+     */
+    public void delete (Item item) {
+        for(int index = 0; index < this.position; index++) {
+            if (this.items[index].getId().equals(item.getId())) {
+                System.arraycopy(this.items, index + 1, this.items, index, this.items.length - 1 - index);
+                break;
+                //this.items[this.items.length - 1] = null;
+            }
+        }
+    }
+    /**
+     * Метод update  обновляет объект из массива.
+     * @param item - объект item.
+     */
+    public void update (Item item) {
+        for(int index = 0; index < this.position; index++) {
+            if (this.items[index].getId().equals(item.getId())){
+                this.items[index] = item;
+                break;
+            }
+        }
+    }
+    /**
+     * Метод findByName  ищет заявку по имени.
+     * @param key - название.
+     * @return возвращаем обрезанный архив result.
+     */
+    public Item[] findByName(String key) {
+        Item[] result = new Item[this.position];
+        int count = 0;
+        for (int index = 0; index < this.position; index++) {
+            if (this.items[index].getName().equals(key)) {
+                result[count] = this.items[index];
+                count++;
+            }
+        }
+        result = Arrays.copyOf(result, count);
+        return result;
+    }
+    /**
+     * Метод findById ищем заявку по ID.
+     * @param id передаем ID заявки.
+     * @return возвращаем result - найденную заявку.
+     */
     protected Item findById (String id) {
         Item result = null;
         for (Item item : items) {
@@ -29,9 +85,12 @@ public class Tracker {
     String generateId () {
        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
     }
-
+    /**
+     * Метод getAll возвращаем копию массива без null.
+     * @return возвращаем result.
+     */
     public Item[] getAll () {
-        Item[] result = new Item[position];
+        Item[] result = new Item[this.position];
         for (int index = 0; index != this.position; index++) {
             result[index] = this.items[index];
         }
--- chapter_002/src/test/java/ru/job4j/tracker/start/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/start/TrackerTest.java
@@ -3,10 +3,11 @@ package ru.job4j.tracker.start;
 import ru.job4j.tracker.models.Item;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 /**
- * Class Tracker.
+ * Class TrackerTest.
  * @author alekseev
  * @since 1.0
  */
@@ -30,8 +31,8 @@ public class TrackerTest {
     public void testMethodAdd() {
         Tracker tracker = new Tracker();
         Item item = new Item("test1","testDescription",123L);
-        Item result = tracker.add(item);
-        assertThat(result, is(item));
+        tracker.add(item);
+        assertThat(item.getId(), is(notNullValue()));
     }
 
     /**
@@ -41,9 +42,50 @@ public class TrackerTest {
     public void testMethodFindById() {
         Tracker tracker = new Tracker();
         Item item = new Item("test1","testDescription",123L);
-        item.setId("first");
-        Item result = tracker.findById("first");
-        assertThat(result, is("First"));
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result, is(item));
     }
-
+    /**
+     * Тестируем класс трекер - тестируем метод update.
+     */
+    @Test
+    public void testMethodUpdate() {
+        Tracker tracker = new Tracker();
+        Item itemFirst = new Item("test1","testDescription",123L);
+        tracker.add(itemFirst);
+        Item itemTwo = new Item("test2", "testDesc2", 123L);
+        itemTwo.setId(itemFirst.getId());
+        tracker.update(itemTwo);
+        assertThat(tracker.findById(itemTwo.getId()), is(itemTwo));
+    }
+    /**
+     * Тестируем класс трекер - тестируем метод update.
+     */
+    @Test
+    public void testMethodDelete() {
+        Tracker tracker = new Tracker();
+        Item itemOne = new Item("test1","testDescription1",121);
+        Item itemTwo = new Item("test2","testDescription2",122);
+        Item itemThree = new Item("test3","testDescription3",123);
+        tracker.add(itemOne);
+        tracker.add(itemTwo);
+        tracker.add(itemThree);
+        tracker.delete(itemOne);
+        assertThat(itemTwo, is(tracker.getAll()[0]));
+    }
+    /**
+     * Тестируем класс трекер - тестируем метод FindAll.
+     */
+    @Test
+    public void testMethodFindByName () {
+        Tracker tracker = new Tracker();
+        Item itemOne = new Item("test1","testDescription1",121);
+        Item itemTwo = new Item("test2","testDescription2",122);
+        Item itemThree = new Item("test3","testDescription3",123);
+        tracker.add(itemOne);
+        tracker.add(itemTwo);
+        tracker.add(itemThree);
+        assertThat(tracker.findByName("test3"), is(tracker.findByName(itemThree.getName())));
+    }
 }
