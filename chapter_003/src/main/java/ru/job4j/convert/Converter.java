4f134c351af2c001026d9a0791402ac388f84bbb
Kirill Alekseev <alekseev.kirill@outlook.com>
2017-09-12 00:08:47 +0400

    #complete task #10035

---
+++ chapter_003/src/main/java/ru/job4j/convert/Converter.java
@@ -0,0 +1,36 @@
+package ru.job4j.convert;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by aleks on 07.09.2017.
+ */
+public class Converter {
+    public List<Integer> toList (int[][] array) {
+        List<Integer> list = new LinkedList<>();
+        for (int index[] : array){
+            for (int arr : index){
+            list.add(arr);
+            }
+        }
+        return list;
+    }
+    public int[][] toArray (List<Integer> list, int rows) {
+        int size = list.size();
+        int index = 0;
+        int[][] array = new int[rows][size/rows + size%rows];
+        for (int indexRows = 0; indexRows < rows; indexRows++){
+            for (int indexLine = 0; indexLine < size/rows + size%rows; indexLine++){
+                if (index < size) {
+                    array[indexRows][indexLine] = list.get(index);
+                }else {
+                    array[indexRows][indexLine] = 0;
+                }
+                index++;
+            }
+        }
+
+    return array;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/convert/ConverterTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.convert;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by aleks on 07.09.2017.
+ */
+public class ConverterTest {
+    @Test
+    public void convertArrayToList() {
+        int[][] array = {{1, 2, 3, 4, 5, 6},{1, 2, 3, 4, 5, 6}};
+        Converter converter = new Converter();
+        List <Integer> result = converter.toList(array);
+        assertThat(result, is(-1));
+    }
+    @Test
+    public void convertListToArray() {
+        Converter converter = new Converter();
+        List <Integer> list = new LinkedList<>();
+        for (int index = 0; index < 7; index++){
+            list.add(index);
+        }
+        int[][] result = converter.toArray(list, 3);
+        assertThat(result, is(-1));
+    }
+}
