1ece7a799531130bbfdba42d64c87b8d504e91c8
Kirill Alekseev <alekseev.kirill@outlook.com>
2017-11-23 01:50:26 +0300

    #stable #3 task #test task

--- listsorting/src/main/java/ru/job4j/Listsort.java
+++ listsorting/src/main/java/ru/job4j/Listsort.java
@@ -26,13 +26,25 @@ public class Listsort {
                 new Comparator<List<String>>() {
                     @Override
                     public int compare(List<String> left, List<String> right) {
-                        return left.get(0).compareTo(right.get(1));
-                        //return right.get(1).compareTo(left.get(0));
+                        int min = left.size() > right.size() ? right.size() : left.size();
+                        for (int index = 0; index != min; index++) {
+                            int cmp = left.get(index).compareTo(right.get(index));
+
+                            if (cmp != 0) {
+                                return cmp;
+                            }
+                            //return left.get(0).compareTo(right.get(1));
+                            //return right.get(1).compareTo(left.get(0));
+                        }
+                        if (left.size() > right.size()){
+                            return 0;
+                        }
+                        return 1;
                     }
                 }
         );
-        data.add(Arrays.asList("2", "2", "1"));
         data.add(Arrays.asList("3", "2", "1"));
+        data.add(Arrays.asList("2", "2", "1"));
         //data.add(Arrays.asList("2", "2", "2"));
         data.add(Arrays.asList("1", "2", "3"));
         System.out.println(data);
