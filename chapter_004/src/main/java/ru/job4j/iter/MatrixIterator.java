ad5a5835edd398097f01ba7116137b0b6e86a829
Kirill Alekseev <alekseev.kirill@outlook.com>
2017-12-05 14:41:34 +0300

    #update task #2 #9539

--- chapter_004/src/main/java/ru/job4j/iter/MatrixIterator.java
+++ chapter_004/src/main/java/ru/job4j/iter/MatrixIterator.java
@@ -26,21 +26,17 @@ public class MatrixIterator implements Iterator {
 
     @Override
     public Object next() {
-        if (indexOne < array.length) {
-            if (array[indexOne].length > indexTwo) {
-                indexTwo++;
-                if (array[indexOne].length == indexTwo) {
-                    int index = indexTwo - 1;
-                    indexTwo = 0;
-                    return array[indexOne++][index];
+                if(hasNext() == true) {
+                    indexTwo++;
+                    if (array[indexOne].length == indexTwo) {
+                        int index = indexTwo - 1;
+                        indexTwo = 0;
+                        return array[indexOne++][index];
+                    }
+                    return array[indexOne][indexTwo - 1];
+                } else {
+                    throw new NoSuchElementException("No such element.");
                 }
-                return array[indexOne][indexTwo - 1];
-            }
-
-        } else {
-            throw new NoSuchElementException("No such element.");
-        }
-        return 0;
         //return array[indexOne][indexTwo++];
     }
 }
