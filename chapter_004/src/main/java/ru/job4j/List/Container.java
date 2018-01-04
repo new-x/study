7205b16832cc25203582721d163f8ba0ecda4b93
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-01-04 15:22:32 +0300

    #task update #158

--- chapter_004/src/main/java/ru/job4j/List/Container.java
+++ chapter_004/src/main/java/ru/job4j/List/Container.java
@@ -1,25 +1,34 @@
 package ru.job4j.List;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class Container<E> implements SimpleContainer<E> {
-    private int modCount = 0;
+    private int modCount;
     private Object[] objects;
     private int position = 0;
+    private int index = 0;
 
-    public Container(int size) {
+    public Container(int size, int modCount) {
         this.objects = new Object[size];
+        this.modCount = modCount;
     }
 
     @Override
     public void add(E e) {
-        if (this.objects.length > position){
-            this.objects[position++] = e;
+        if (modCount > 0) {
+            if (this.objects.length > position) {
+                this.objects[position++] = e;
+            } else {
+                this.objects = Arrays.copyOf(this.objects, position + 3);
+                modCount--;
+                this.objects[position++] = e;
+            }
         } else {
-            this.objects = Arrays.copyOf(this.objects, position + 3);
-            this.objects[position++] = e;
+            throw new ConcurrentModificationException("Mod count exception.");
         }
+
     }
 
     @Override
@@ -32,17 +41,23 @@ public class Container<E> implements SimpleContainer<E> {
 
     @Override
     public Iterator<E> iterator() {
-        Iterator<E> it = new Iterator<E>() {
+        Object[] array = this.objects;
+        return new Iterator<E>() {
             @Override
             public boolean hasNext() {
-                return false;
+                if (array[index] != null) {
+                    return true;
+                }
+                    return false;
             }
 
             @Override
             public E next() {
+                if (hasNext()) {
+                    return (E) array[index++];
+                }
                 return null;
             }
         };
-        return null;
     }
 }
--- chapter_004/src/test/java/ru/job4j/List/ContainerTest.java
+++ chapter_004/src/test/java/ru/job4j/List/ContainerTest.java
@@ -2,6 +2,8 @@ package ru.job4j.List;
 
 import org.junit.Test;
 
+import java.util.ConcurrentModificationException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,12 +11,30 @@ public class ContainerTest {
 
     @Test
     public void whenAddItemIsCompete() {
-        Container<String> container = new Container<String>(3);
+        Container<String> container = new Container<String>(3, 2);
         container.add("Маша");
         container.add("Даша");
         container.add("Саша");
         container.add("Паша");
-        assertThat(container.get(3), is("Паша"));
+        assertThat(container.iterator().hasNext(), is(true));
+        assertThat(container.iterator().next(), is("Маша"));
+        assertThat(container.iterator().hasNext(), is(true));
+        assertThat(container.iterator().next(), is("Даша"));
+        assertThat(container.iterator().hasNext(), is(true));
+        assertThat(container.iterator().next(), is("Саша"));
+        assertThat(container.iterator().hasNext(), is(true));
+        assertThat(container.iterator().next(), is("Паша"));
+        assertThat(container.iterator().hasNext(), is(false));
     }
 
+    @Test (expected = ConcurrentModificationException.class)
+    public void whenModCountException(){
+        Container<String> container = new Container<String>(3, 1);
+        container.add("Маша");
+        container.add("Даша");
+        container.add("Саша");
+        container.add("Паша");
+        container.add("Паша");
+
+    }
 }
