badb2f484f37c69f106b475d8d4f69ad86204dcc
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-02-18 15:53:00 +0300

    #task create #1005

---
+++ chapter_004/src/test/java/ru/job4j/Map/UserTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.Map;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void whenShud(){
+        Map map = new HashMap();
+        User user1 = (new User("Andre", 1, new GregorianCalendar(2018, 1, 2)));
+        User user2 = (new User("Andre", 1, new GregorianCalendar(2018, 1, 2)));
+        map.put(user1, "One");
+        map.put(user2, "One");
+        System.out.println(user1.hashCode() + " "  + user2.hashCode());
+        System.out.println(user1.equals(user2));
+        assertThat(user1, is(user2));
+    }
+
+
+}
