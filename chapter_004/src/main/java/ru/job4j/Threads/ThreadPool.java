19d15c7f86349a9407fcf79a481a0ef6eb846f21
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-05-10 23:25:13 +0300

    update task #1099

--- chapter_004/src/main/java/ru/job4j/Threads/ThreadPool.java
+++ chapter_004/src/main/java/ru/job4j/Threads/ThreadPool.java
@@ -1,7 +1,9 @@
 package ru.job4j.Threads;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import net.jcip.annotations.GuardedBy;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by Alekseev Kirill.
@@ -10,13 +12,63 @@ import java.util.concurrent.Executors;
  */
 
 public class ThreadPool {
-    public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        ThreadPool pool = new ThreadPool();
-        for (int index = 0; index < 12; index++) {
-            threadPool.submit(new Work());
+
+    private static final int COUNT_CORES = Runtime.getRuntime().availableProcessors();
+    private static final int TASK_LIMIT = 10;
+    @GuardedBy("this")
+    private Queue<Work> taskQueue = new LinkedList<>();
+    private Thread[] allThreads;
+    private boolean check = true;
+
+    public void stratThreads() {
+        this.allThreads = new Thread[COUNT_CORES];
+        for (int index = 0; index < TASK_LIMIT; index++) {
+            add(new Work());
         }
-        threadPool.shutdown();
+        for (int index = 0; index < COUNT_CORES; index++) {
+            this.allThreads[index] = new Thread(() -> {
+                while (check) {
+                    if (!this.taskQueue.isEmpty()) {
+                        Work work = this.taskQueue.poll();
+                        if (work != null) {
+                            work.work();
+                        }
+                    } else {
+                        this.check = false;
+                    }
+                }
+            }
+            );
+            this.allThreads[index].start();
+        }
+    }
+
+    public void waitThreads() {
+        for (Thread thread : allThreads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void add(Work work) {
+        synchronized (this) {
+            this.taskQueue.offer(work);
+        }
+    }
+
+    public static class Work {
+        public void work() {
+            try {
+                Thread.sleep((long) (Math.random() * 1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName());
+        }
     }
 }
+
 
---
+++ chapter_004/src/test/java/ru/job4j/Threads/ThreadPoolTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.Threads;
+
+import org.junit.Test;
+import ru.job4j.Threads.ThreadPool.Work;
+
+/**
+ * Created by Alekseev Kirill.
+ * Package name: ru.job4j.Threads
+ * Create data: 29.04.2018 21:24
+ */
+
+public class ThreadPoolTest {
+
+    @Test
+    public void whenAllThreadsWork() throws InterruptedException {
+        ThreadPool th = new ThreadPool();
+        th.stratThreads();
+        th.waitThreads();
+
+    }
+}
