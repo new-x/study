454b1aa4c993ea8f4cf4a10f722f1f477fb947ce
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-05-14 13:30:36 +0300

    update task #1099

--- chapter_004/src/main/java/ru/job4j/Threads/ThreadPool.java
+++ chapter_004/src/main/java/ru/job4j/Threads/ThreadPool.java
@@ -1,6 +1,7 @@
 package ru.job4j.Threads;
 
 import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,15 +11,14 @@ import java.util.Queue;
  * Package name: ru.job4j.Threads
  * Create data: 08.04.2018 20:49
  */
-
+@ThreadSafe
 public class ThreadPool {
 
     private static final int COUNT_CORES = Runtime.getRuntime().availableProcessors();
     private static final int TASK_LIMIT = 10;
     @GuardedBy("this")
-    private Queue<Work> taskQueue = new LinkedList<>();
+    private volatile Queue<Work> taskQueue = new LinkedList<>();
     private Thread[] allThreads;
-    private boolean check = true;
 
     public void stratThreads() {
         this.allThreads = new Thread[COUNT_CORES];
@@ -26,22 +26,21 @@ public class ThreadPool {
             add(new Work());
         }
         for (int index = 0; index < COUNT_CORES; index++) {
-            synchronized (this) {
-                this.allThreads[index] = new Thread(() -> {
-                    while (check) {
-                        if (!this.taskQueue.isEmpty()) {
-                            Work work = this.taskQueue.poll();
-                            if (work != null) {
-                                work.work();
-                            }
-                        } else {
-                            this.check = false;
-                        }
+            this.allThreads[index] = new Thread(() -> {
+                while (true) {
+                    if (this.taskQueue.isEmpty()) {
+                        break;
                     }
+                    pollWork();
                 }
-                );
-            }
+            });
             this.allThreads[index].start();
+        }
+    }
+
+    public synchronized void pollWork() {
+        if (this.taskQueue.peek() != null) {
+            this.taskQueue.poll().work();
         }
     }
 
@@ -55,16 +54,14 @@ public class ThreadPool {
         }
     }
 
-    public void add(Work work) {
-        synchronized (this) {
-            this.taskQueue.offer(work);
-        }
+    public synchronized void add(Work work) {
+        this.taskQueue.offer(work);
     }
 
     public static class Work {
         public void work() {
             try {
-                Thread.sleep((long) (Math.random() * 1000));
+                Thread.sleep((long) (Math.random() * 100));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
