bf9c7690522205c67e294e26c3f397cd2af64aa9
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-05-14 21:34:14 +0300

    update task #50450

--- testquest1/src/main/java/ru/job4j/WorldChecking.java
+++ testquest1/src/main/java/ru/job4j/WorldChecking.java
@@ -8,36 +8,35 @@ import java.util.Map;
  * Package name: ru.job4j
  * Create data: 03.05.2018 23:28
  */
-// мама маам
+
 public class WorldChecking {
     public boolean checkWorld(String worldOne, String worldTwo) {
         HashMap<Character, Integer> mapOne = new HashMap<>();
         char[] worldTwoSymbols = worldTwo.toCharArray();
+
         if (worldOne.length() != worldTwo.length()) {
-        return false;
+            return false;
         }
+
         for (int index = 0; index < worldOne.length(); index++) {
-            if (mapOne.containsKey(worldOne.charAt(index))) {
-                Integer valueOne = mapOne.get(worldOne.charAt(index));
-                mapOne.replace(worldOne.charAt(index), ++valueOne);
-            } else {
+            mapOne.computeIfPresent(worldOne.charAt(index), (key, value) -> ++value);
+            if (!mapOne.containsKey(worldOne.charAt(index))) {
                 mapOne.put(worldOne.charAt(index), 1);
             }
         }
+
         for (Map.Entry map : mapOne.entrySet()) {
-            char symbol = (char) map.getKey();
-            Integer countSymbols = (Integer) map.getValue();
             Integer count = 0;
             for (char ch : worldTwoSymbols) {
-                if (symbol == ch) {
+                if ((char) map.getKey() == ch) {
                     count++;
                 }
             }
-            if (countSymbols != count) {
+            if (map.getValue() != count) {
                 return false;
             }
         }
-        return  true;
+        return true;
     }
 
 }
