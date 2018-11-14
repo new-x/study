31434e50c89f5351c4b891d0b547327c6346360c
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-11-14 19:06:49 +0300

    test task Scratch

---
+++ scratch/src/main/java/ru/scratch/Scratch.java
@@ -0,0 +1,29 @@
+package ru.scratch;
+
+public class Scratch {
+    public void sort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            sort(A, p, q);
+            sort(A, q + 1, r);
+            this.merge(A, p, q, r);
+        }
+    }
+
+    public void merge(int[] arr, int p, int q, int r) {
+        int firstArrayPosition = p - 1;
+        int firstArrayLastIndex = q - 1;
+        int secondArrayPosition = q;
+        int secondArrayLastIndex = r - 1;
+        int workSize = secondArrayLastIndex - firstArrayPosition;
+        int[] result = new int[workSize + 1];
+        for(int i = 0; i <= workSize; i++) {
+            result[i] =
+                    (secondArrayPosition > secondArrayLastIndex)
+                            || (firstArrayPosition <= firstArrayLastIndex
+                            && arr[firstArrayPosition] < arr[secondArrayPosition])
+                            ? arr[firstArrayPosition++] : arr[secondArrayPosition++];
+        }
+        System.arraycopy(result, 0, arr, p - 1, result.length);
+    }
+}
