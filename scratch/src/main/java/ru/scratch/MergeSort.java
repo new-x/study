2c765449652a59095b8c3df726abbeef17a240a2
Kirill Alekseev <alekseev.kirill@outlook.com>
2018-11-14 19:42:35 +0300

    merge sort update

--- scratch/src/main/java/ru/scratch/MergeSort.java
+++ scratch/src/main/java/ru/scratch/MergeSort.java
@@ -1,6 +1,13 @@
 package ru.scratch;
 
-public class Scratch {
+/*
+ * Merge Sorter
+ * @author Alexeev Kirill
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MergeSort {
     public void sort(int[] A, int p, int r) {
         if (p < r) {
             int q = (p + r) / 2;
@@ -17,7 +24,7 @@ public class Scratch {
         int secondArrayLastIndex = r - 1;
         int workSize = secondArrayLastIndex - firstArrayPosition;
         int[] result = new int[workSize + 1];
-        for(int i = 0; i <= workSize; i++) {
+        for (int i = 0; i <= workSize; i++) {
             result[i] =
                     (secondArrayPosition > secondArrayLastIndex)
                             || (firstArrayPosition <= firstArrayLastIndex
---
+++ scratch/src/test/java/ru/scratch/MergeSortTest.java
@@ -0,0 +1,52 @@
+package ru.scratch;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/*
+ * Merge Sort Test
+ * @author Alexeev Kirill
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MergeSortTest {
+
+    /*
+     * Even array sorting test
+     */
+    @Test
+    public void evenTest() {
+        MergeSort mergeSort = new MergeSort();
+        int[] array = new int[]{5, 2, 4, 6, 1, 3, 2, 6};
+        int[] expectedArray = new int[]{1, 2, 2, 3, 4, 5, 6, 6};
+        mergeSort.sort(array, 1, 8);
+        assertThat(array, is(expectedArray));
+    }
+
+    /*
+     * Odd array sorting test
+     */
+    @Test
+    public void oddTest() {
+        MergeSort mergeSort = new MergeSort();
+        int[] array = new int[]{9, 12, 4, 11, 8, 3, 1, 5, 2, 10, 7, 6, 13};
+        int[] expectedArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        mergeSort.sort(array, 1, 13);
+        assertThat(array, is(expectedArray));
+    }
+    /*
+    * Test sort of the middle
+    */
+    @Test
+    public void middleTest() {
+        MergeSort mergeSort = new MergeSort();
+        int[] array = new int[]{5, 2, 4, 1, 3, 6, 8, 7, 9, 10};
+        int[] expectedArray = new int[]{5, 2, 1, 3, 4, 6, 8, 7, 9, 10};
+        mergeSort.sort(array, 3, 7);
+        assertThat(array, is(expectedArray));
+    }
+
+}
